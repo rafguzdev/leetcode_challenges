5d1faafa483329feaa3561bd9f8aff19d372afe4
rafguzdev <rafguzdev@gmail.com>
2022-07-11 20:12:40 +0200

    Two Sum

--- 001_Two_Sum/main.py
+++ 001_Two_Sum/main.py
@@ -1,11 +1,26 @@
-
+# SOLUTION 1
 def fun(nums, target):
+    store = {}
     for idx, num in enumerate(nums):
-        for idy, num2 in enumerate(nums[idx+1:]):
-            if num + num2 == target:
-                print(num, num2)
-                print([idx, nums.index(num2, idx+1)])
-                return True
+        if target - num in store:
+            return [store[target-num], idx]
+        elif num not in store:
+            store[num] = idx
 
 
-fun(nums=[80, 80, 11, 15, 12, 22, 145], target=160)
+print(fun(nums=[2, 7, 2, 15, 11], target=26))
+
+# SOLUTION 2
+# def fun(nums, target):
+#     for idx, num in enumerate(nums):
+#         new = target - num
+#         idy = nums[idx+1:].index(new) + idx+1 if new in nums[idx+1:] else -1
+#         if idy > -1:
+#             return [idx, idy]
+
+# SOLUTION 3
+# def fun(nums, target):
+#     for idx, num in enumerate(nums):
+#         for idy, num2 in enumerate(nums[idx + 1:]):
+#             if num + num2 == target:
+#                 return [idx, nums.index(num2, idx + 1)]
